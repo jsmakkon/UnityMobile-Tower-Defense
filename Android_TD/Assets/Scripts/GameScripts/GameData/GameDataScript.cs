b577f28284965fea30730926a34f10ad971023cd
Joni Makkonen <jsmakkon@gmail.com>
2016-03-09 03:52:46 +0200

    Added few script files for gamedata

---
+++ Android_TD/Assets/Scripts/GameScripts/GameData/GameDataScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Data storage class for game information, meaning number of waves, 
+   number of monsters per wave...*/
+public class GameDataScript : MonoBehaviour {
+
+	
+}
---
+++ Android_TD/Assets/Scripts/GameScripts/GameData/WaveScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using EnemyType = EnemyScript.EnemyType;
+
+public class WaveScript {
+
+    private List<EnemyType> enemies;
+
+    public WaveScript()
+    {
+        enemies = new List<EnemyType>();
+        
+    }
+
+}
